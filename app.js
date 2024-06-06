a78f4f7ffe46813a2a90a98ba6a942b4bcda8e2c
Bilal <mohammadbilal91112@gmail.com>
2024-06-07 02:57:38 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const fullName = document.querySelector('#full-name')
+
+
+function click(){
+    console.log('hello')
+}
+
+
+alert(please)
+
+
